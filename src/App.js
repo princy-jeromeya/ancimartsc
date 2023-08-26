1e30fb92acdeaeac1351389ca761cdb87914b38b
princy-jeromeya <princyjeromeya2000@gmail.com>
2023-08-26 14:00:09 +0530

    construction-website

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import data from "./components/Data/Data";
+import Header from "./components/font/Header";
+
+const App=()=>{
+  const {productItems}=data;
+  return <div>
+    <Header/>
+  </div>
+}
+
+export default App;
---
+++ src/components/back/Data/Data.js
@@ -0,0 +1,100 @@
+const data = {
+    productItems:[
+        {
+            id:"1",
+            name:"Steel Shuttering Plate",
+            price:1000,
+            image:"./pics/"
+        },
+        {
+            id:"2",
+            name:"Base plate",
+            price:1000,
+            image:""
+        },
+        {
+            id:"3",
+            name:"Jack base plate",
+            price:1000,
+            image:""
+        },
+        {
+            id:"4",
+            name:"Adujustable base jack",
+            price:1000,
+            image:""
+        },
+        {
+            id:"5",
+            name:"Chaali",
+            price:1000,
+            image:""
+        },
+        {
+            id:"6",
+            name:"Steel channel",
+            price:1000,
+            image:""
+        },
+        {
+            id:"7",
+            name:"Props",
+            price:1000,
+            image:""
+        },
+        {
+            id:"8",
+            name:"Inner pipe",
+            price:1000,
+            image:""
+        },
+        {
+            id:"9",
+            name:"Outer pipe",
+            price:1000,
+            image:""
+        },
+        {
+            id:"10",
+            name:"Standard",
+            price:1000,
+            image:""
+        },
+        {
+            id:"11",
+            name:"ledger",
+            price:1000,
+            image:""
+        },
+        {
+            id:"12",
+            name:"Cuplock",
+            price:1000,
+            image:""
+        },
+        {
+            id:"13",
+            name:"Pin",
+            price:1000,
+            image:""
+        },
+        {
+            id:"14",
+            name:"Clamp",
+            price:1000,
+            image:""
+        },
+        {
+            id:"15",
+            name:"Sikanja",
+            price:1000,
+            image:""
+        },
+        {
+            id:"16",
+            name:"Sikanja patti",
+            price:1000,
+            image:""
+        }
+    ]
+}
